e63ae3c3e45f4eff22702687f785e059279ffce1
Adiihere12 <adityajassrotia1999@gmail.com>
2024-11-04 13:52:45 +0530

    home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import LatestCollection from '../Components/LatestCollection'
+import Bestseller from '../Components/Bestseller'
+import Ourpolicy from '../Components/Ourpolicy'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+      <Bestseller />
+      <Ourpolicy />
+      
+    </div>
+  )
+}
+
+export default Home
